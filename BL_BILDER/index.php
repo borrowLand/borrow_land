8c87ca9ad929f891030b50dd340cf5560f6dea0a
REG_W <thewss@gmx.de>
2014-02-16 13:21:04 +0100

    Initialer Upload

---
+++ BL_BILDER/index.php
@@ -0,0 +1,170 @@
+<?
+session_start();
+
+
+//System 
+/////////////////////////////////////////////
+$includeName="../_00_basic_check.inc.php";
+if (file_exists($includeName))
+{
+require($includeName);
+}	
+else
+{
+echo '<br><br><div class="meldung_fehler"><img src="../BL_BILDER/Meldungen_Warning.png"> <br>CHECK_ALL_LOAD_01</div><br><br>';	
+exit();
+}
+
+/////////////////////////////////////////////
+
+//Funktionen 
+/////////////////////////////////////////////
+$includeName="../_00_basic_func.inc.php";
+if (file_exists($includeName))
+{
+require($includeName);
+}	
+else
+{
+echo '<br><br><div class="meldung_fehler"><img src="../BL_BILDER/Meldungen_Warning.png"> <br>FU_ALL_LOAD_01</div><br><br>';	
+exit();
+}
+
+/////////////////////////////////////////////
+
+
+//Datenbank Verbindung
+/////////////////////////////////////////////
+$includeName="../_01_basic_db.inc.php";
+if (file_exists($includeName))
+{
+require($includeName);
+}	
+else
+{
+echo '<br><br><div class="meldung_fehler"><img src="../BL_BILDER/Meldungen_Warning.png"> <br>DB_FU_LOAD_01</div><br><br>';	
+exit();
+}
+
+/////////////////////////////////////////////
+
+
+//Sessions
+/////////////////////////////////////////////
+$includeName="../_01_basic_sess.inc.php";
+if (file_exists($includeName))
+{
+require($includeName);
+}	
+else
+{
+echo '<br><br><div class="meldung_fehler"><img src="../BL_BILDER/Meldungen_Warning.png"> <br>SESS_FU_LOAD_01</div><br><br>';	
+exit();
+}
+
+/////////////////////////////////////////////
+
+
+?>
+
+<!DOCTYPE html> 
+<html>
+<head>
+<?
+echo eval(welcheEinstellung("SE_headBereich"));
+?>
+</head>
+
+
+<body>
+<NOSCRIPT>
+<br><br><br><br><div class="meldung_fehler"><img src="../BL_BILDER/Meldungen_Warning.png">Diese Webseite läuft leider nur, wenn Sie <a href="http://de.wikipedia.org/wiki/Javascript" target="_blank">Javascript</a> zulassen. <br>Bitte aktivieren Sie diesen technischen Standard in Ihrem Browser, Danke!</div>
+<br><br>
+</NOSCRIPT>	
+		
+<section id="page"> <!-- Defining the #page section with the section tag -->
+
+
+            <header> <!-- Defining the header section of the page with the appropriate tag -->
+            
+            <hgroup>
+			<h1><a href="../index.php" alt="Startseite" title="Startseite"><img src="../BL_BILDER/start_00.png"></a> <a href="../index.php" alt="Startseite" title="Startseite">borrow land</a></h1>
+           	</hgroup>
+			
+
+			
+            </header>
+            <section id="articles"> 
+
+<article id="articleLogin1"> <!-- The new article tag. The id is supplied so it can be scrolled into view. -->
+	<h2>Hinweis</h2>
+	
+	<div class="line"></div>
+	
+	<div class="articleBody clear">
+
+	<figure> <!-- The figure tag marks data (usually an image) that is part of the article -->
+	<a href="../"><img src="../BL_BILDER/unbekannteSeite.jpg" title="unbekannte Seite" width="200" height="95" /></a>
+	</figure>
+		
+		<p>Bitte klicken Sie <a href="../index.php">hier</a> um zur Startseite zu gelangen. </p>
+  </div>
+
+</article>
+			
+
+
+            </section>
+
+			
+			
+			
+			
+			
+		
+		 <?
+		 if ($_SESSION["SE_GooTrans"]=="1")
+		 {
+					// Start Google Translate -->
+   					echo $_SESSION["SE_GooTransScript"];
+					// Ende Google Translate -->
+		}
+		?>		
+
+			
+<?
+//Footer 
+/////////////////////////////////////////////
+$includeName="../_00_basic_footer.php";
+if (file_exists($includeName))
+{
+include_once($includeName);
+}	
+else
+{
+echo '<br><br><div class="meldung_fehler"><img src="../BL_BILDER/Meldungen_Warning.png"> <br>FOOTER_FU_LOAD</div><br><br>';	
+exit();
+}
+
+/////////////////////////////////////////////			
+?>  
+	        
+ 
+		 
+
+        
+<!-- JavaScript Includes -->
+<?
+echo nl2br($_SESSION["SE_jQuerUI"]);
+?>  
+
+
+
+
+<?
+echo utf8_encode($_SESSION["SE_EndeIndex"]);
+?>
+
+<br><br><br><br><br><br>
+</body>
+</html>
